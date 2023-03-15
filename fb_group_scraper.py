7b66b9bd54a6ec0e97dfcaca037982001c04ccee
kmotte <kmotte@gmail.com>
2023-03-15 08:52:55 -0700

    Update fb_group_scraper.py

--- fb_group_scraper.py
+++ fb_group_scraper.py
@@ -1,67 +1,56 @@
-import os
-import json
-import datetime
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 import streamlit as st
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from facebook import GraphAPI
+import os
 
-# Set your Facebook access token and Google API credentials
-FB_ACCESS_TOKEN = 'YOUR_FACEBOOK_ACCESS_TOKEN'
-GOOGLE_SERVICE_ACCOUNT_FILE = 'your-google-service-account.json'
-
-# Initialize Facebook Graph API and Google Sheets API
-graph = GraphAPI(access_token=FB_ACCESS_TOKEN, version='3.0')
-google_credentials = service_account.Credentials.from_service_account_file(GOOGLE_SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
-sheets_api = build('sheets', 'v4', credentials=google_credentials)
-
-# Google Sheets settings
-SPREADSHEET_ID = 'YOUR_SPREADSHEET_ID'
-RANGE_NAME = 'Sheet1!A1:G'
-
-# Utility functions
-def get_group_posts(group_id, since_date):
-    # Fetch group posts for the specified date
-    posts = graph.get_connections(id=group_id, connection_name='feed', since=since_date)
-    return posts['data']
-
-def add_posts_to_google_sheet(posts):
-    # Add posts to Google Sheet
-    data = [[post['id'], post['message'], post['created_time'], '', '', '', ''] for post in posts]
-    body = {'values': data}
-    sheets_api.spreadsheets().values().append(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, valueInputOption='RAW', insertDataOption='INSERT_ROWS', body=body).execute()
-
-def perform_sentiment_analysis():
-    # Perform sentiment analysis using OpenAI API
+# The following function is assumed to be provided
+def google_sheets_add_row(sheets_url, sheet_name, data):
     pass
 
-def write_response_to_post(post_id, response_message):
-    # Write response to the post
-    graph.put_object(parent_object=post_id, connection_name='comments', message=response_message)
+def get_facebook_group_posts(group_url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    }
 
-# Streamlit app
-st.title("Facebook Group Scraper")
+    response = requests.get(group_url, headers=headers)
 
-group_id = st.text_input("Enter the Facebook Group ID to scrape:")
-date = st.date_input("Select the date to fetch posts:", datetime.date.today())
+    if response.status_code != 200:
+        return []
 
-if st.button("Scrape Group"):
-    # Scrape group and add posts to Google Sheet
-    posts = get_group_posts(group_id, date)
-    add_posts_to_google_sheet(posts)
+    soup = BeautifulSoup(response.text, "html.parser")
+    posts = soup.find_all("div", class_="_5pcr userContentWrapper")
 
-    # Perform sentiment analysis
-    perform_sentiment_analysis()
+    post_texts = []
 
-    # Display the scraped posts
-    df = pd.DataFrame(posts, columns=['id', 'message', 'created_time'])
-    st.dataframe(df)
+    for post in posts:
+        post_text = post.find("div", class_="userContent").text.strip()
+        post_texts.append(post_text)
 
-    # Write a response to a post
-    post_id = st.text_input("Enter the post ID to respond to:")
-    response_message = st.text_input("Enter your response:")
+    return post_texts
 
-    if st.button("Submit Response"):
-        write
+def main():
+    st.title("Facebook Group Posts to Google Sheets")
+
+    group_url = st.text_input("Enter the Facebook Group URL:")
+
+    if group_url:
+        posts = get_facebook_group_posts(group_url)
+
+        if posts:
+            st.write("Posts found:")
+            for post in posts:
+                st.write(post)
+
+            sheets_url = "https://docs.google.com/spreadsheets/d/1Atszy9fGR6WYxMsTodoWzDLZviQKb54zb0fzfmE773g/edit#gid=1780586757"
+            sheet_name = "Sheet1"
+
+            for post in posts:
+                google_sheets_add_row(sheets_url, sheet_name, [post])
+
+            st.write("Posts have been added to Google Sheets.")
+        else:
+            st.write("No posts found.")
+
+if __name__ == "__main__":
+    main()
+
